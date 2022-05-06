c543dd24e95d74c9797f66ffc21d0feb104c34f7
Vainius <vmykolaitis1@gmail.com>
2022-05-07 00:11:17 +0300

    Tweaked user routes for login/register

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,9 +1,10 @@
 import { Router, Request, Response } from 'express';
-import { addUser, deleteUser, getAllUsers, getUserById, updateUser } from '../controllers/user';
+import { addUser, deleteUser, getAllUsers, getUserById, login, register, updateUser } from '../controllers/user';
+import { authToken } from '../middlewares/authThoken';
 
 const router = Router();
 
-router.get('/get', async (req: Request, res: Response) => {
+router.get('/get', authToken, async (req: Request, res: Response) => {
   try {
     const users = await getAllUsers();
 
@@ -11,6 +12,48 @@ router.get('/get', async (req: Request, res: Response) => {
       success: true,
       data: users,
     });
+  } catch (error) {
+    console.log(error);
+
+    return res.json({
+      success: false,
+      message: error,
+    });
+  }
+});
+
+router.post('/login', async (req: Request, res: Response) => {
+  try {
+    const { name, password } = req.body;
+
+    const user = await login({ name, password });
+
+    return res.status(user.status).json({
+      success: true,
+      data: user,
+    });
+
+  } catch (error) {
+    console.log(error);
+
+    return res.json({
+      success: false,
+      message: error,
+    });
+  }
+});
+
+router.post('/register', async (req: Request, res: Response) => {
+  try {
+    const { name, password } = req.body;
+
+    const user = await register({ name, password });
+
+    return res.status(user.status).json({
+      success: true,
+      data: user,
+    });
+
   } catch (error) {
     console.log(error);
 

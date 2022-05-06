656e47b609b2e00f24c19b034b969e83271162ad
Vainius <vmykolaitis1@gmail.com>
2022-05-07 00:15:37 +0300

    Added user controller

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -1,4 +1,81 @@
-export const getAllUsers = async () => {};
+import { PrismaClient } from '@prisma/client';
+import jwt from 'jsonwebtoken';
+
+import { User } from '../types/User';
+import { comparePassword, hashPassword } from '../utils/helpers';
+
+const prisma = new PrismaClient();
+
+export const login = async (user: User) => {
+  const { name, password } = user;
+
+  const existingUser = await prisma.user.findFirst({
+    where: {
+      name,
+    },
+  });
+
+  if (!existingUser) {
+    return {
+      status: 400,
+      message: 'User does not exist',
+    };
+  }
+
+  const isPasswordMatching = comparePassword(password, existingUser.password);
+
+  if (isPasswordMatching) {
+    const secret = process.env.ACCESS_TOKEN_SECRET || 'secret';
+    const accessToken = jwt.sign({ name }, secret, { expiresIn: '30 minutes' });
+
+    return {
+      status: 200,
+      accessToken,
+    };
+  }
+
+  return {
+    status: 400,
+    message: 'Incorrect password',
+  };
+};
+
+export const register = async (user: User) => {
+  const { name, password } = user;
+
+  const existingUser = await prisma.user.findFirst({
+    where: {
+      name,
+    },
+  });
+
+  if (existingUser) {
+    return {
+      status: 400,
+      message: 'User already exists',
+    };
+  }
+
+  const hashedPassword = hashPassword(password);
+
+  await prisma.user.create({
+    data: {
+      name,
+      password: hashedPassword,
+    },
+  });
+
+  return {
+    status: 200,
+    message: `${name} successfully registered`,
+  };
+};
+
+export const getAllUsers = async () => {
+  return {
+    message: 'lmao',
+  };
+};
 
 export const getUserById = async () => {};
 

5198b22e92a105ce7220864fd27e1c26f31e19fa
Vainius <vmykolaitis1@gmail.com>
2022-05-07 00:14:37 +0300

    Added cors and additional routes

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
 import express, { Application } from 'express';
-
+import cors from 'cors';
 import routes from './src/config/routes';
 
 import statsRoute from './src/routes/stats';
@@ -8,15 +8,17 @@ import abiRoute from './src/routes/abi';
 import liquidityRoute from './src/routes/liquidity';
 import networkRoute from './src/routes/network';
 import tokenRoute from './src/routes/token';
+import userRoute from './src/routes/user';
 
 const app: Application = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
-const port = process.env.PORT || 3000;
+const port: number | string = process.env.PORT || 3000;
 
-const { stats, project, abi, liquidity, network, token } = routes;
+const { stats, project, abi, liquidity, network, token, user } = routes;
 
 app.use(stats, statsRoute);
 app.use(project, projectRoute);
@@ -24,6 +26,7 @@ app.use(abi, abiRoute);
 app.use(liquidity, liquidityRoute);
 app.use(network, networkRoute);
 app.use(token, tokenRoute);
+app.use(user, userRoute);
 
 app.listen(port, () => {
   console.log(`Titan Chest API server started on port ${port}`);

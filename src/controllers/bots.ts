1d505c70e3b6464c94fe59ba71aaa1179667dd36
Vainius <vmykolaitis1@gmail.com>
2022-05-09 22:12:37 +0300

    Added sanitization for bot_id

--- src/controllers/bots.ts
+++ src/controllers/bots.ts
@@ -1,9 +1,21 @@
 import { PrismaClient, Bots } from '@prisma/client';
+import eventBus from '../utils/eventBus';
+import events from '../utils/events';
 
 const prisma = new PrismaClient();
 
 export const getAllBots = async () => {
   const bots = await prisma.bots.findMany();
+
+  return bots;
+};
+
+export const getAllEnabledBots = async () => {
+  const bots = await prisma.bots.findMany({
+    where: {
+      enabled: true,
+    },
+  });
 
   return bots;
 };
@@ -19,22 +31,32 @@ export const getBotById = async (id: number) => {
 };
 
 export const addBot = async (bot: Bots) => {
+  const sanitizedBot = JSON.parse(JSON.stringify(bot));
+
+  sanitizedBot.bot_id = sanitizedBot?.bot_id?.toString();
+
   const entry = await prisma.bots.create({
-    data: { ...bot },
+    data: { ...sanitizedBot },
   });
 
   return entry;
 };
 
 export const updateBot = async (id: number, bot: Bots) => {
+  const sanitizedBot = JSON.parse(JSON.stringify(bot));
+
+  sanitizedBot.bot_id = sanitizedBot?.bot_id?.toString();
+
   const entry = await prisma.bots.update({
     where: {
       id,
     },
     data: {
-      ...bot,
+      ...sanitizedBot,
     },
   });
+
+  eventBus.emit(events.bots.update, entry);
 
   return entry;
 };

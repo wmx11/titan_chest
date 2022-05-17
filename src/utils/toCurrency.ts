2b623b78d7861772e8f1f4d636809dcc7475dd9c
Vainius <vmykolaitis1@gmail.com>
2022-05-17 21:26:02 +0300

    Added missing toCurrency

---
+++ src/utils/toCurrency.ts
@@ -0,0 +1,14 @@
+const toCurrency = (number: number, maxDigits = 3) => {
+  if (!number) {
+    return;
+  }
+
+  return number.toLocaleString('en-US', {
+    style: 'currency',
+    currency: 'USD',
+    minimumSignificantDigits: 2,
+    maximumSignificantDigits: maxDigits,
+  });
+};
+
+export default toCurrency;

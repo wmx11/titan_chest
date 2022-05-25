98f7e389033363fb449220d44f983a753a8cc447
Vainius <vmykolaitis1@gmail.com>
2022-05-25 22:49:45 +0300

    updated corsOrigin

--- src/config/corsOrigin.ts
+++ src/config/corsOrigin.ts
@@ -1,3 +1,9 @@
-const corsOrigin = ['http://localhost:3000', 'http://titan-chest-ui:3000', 'http://titan-chest-api:2000'];
+const corsOrigin = [
+  'http://localhost:3000',
+  'http://titan-chest-ui:3000',
+  'http://titan-chest-api:2000',
+  'http://localhost',
+  'https://titanchest.com',
+];
 
 export default corsOrigin;

65234f27cb1c47416039f5a7f34852f152d6578e
Vainius <vmykolaitis1@gmail.com>
2022-05-30 12:04:37 +0300

    Tweaked toDecimals helper

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -9,4 +9,10 @@ export const comparePassword = (raw: string, hash: string) => {
   return bcrypt.compareSync(raw, hash);
 };
 
-export const toDecimals = (number: number, decimals = 18) => number / 10 ** decimals;
+export const toDecimals = (number: number | string | null, decimals = 18): number => {
+  if (number) {
+    return parseInt(number, 10) / 10 ** decimals;
+  }
+
+  return 0;
+};

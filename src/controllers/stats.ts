f6dfcb8d3bf8179689b38aaa83935b89f6710722
Vainius <vmykolaitis1@gmail.com>
2022-05-30 12:04:12 +0300

    Added computed fields support

--- src/controllers/stats.ts
+++ src/controllers/stats.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/indent */
 import { PrismaClient, Stats, Prisma } from '@prisma/client';
 import { channels, redisPublish } from '../utils/redisClient';
 
@@ -20,6 +21,7 @@ type Payload = {
   limit: string;
   select: string;
   order: Prisma.SortOrder;
+  compute: string | null;
 };
 
 export const getProjectStatsById = async (payload: Payload) => {
@@ -33,19 +35,48 @@ export const getProjectStatsById = async (payload: Payload) => {
 
   const selections = {};
 
+  type ComputedField = {
+    name: string;
+    compute: boolean;
+    generate: (data: { marketcap: number | null; price: number | null }) => number | null;
+  };
+
+  const computedFields: ComputedField[] = [
+    {
+      name: 'total_supply',
+      compute: !!payload.compute || false,
+      generate: (data): number | null => {
+        if (data.marketcap && data.price) {
+          return data.marketcap / data.price;
+        }
+
+        return null;
+      },
+    },
+  ];
+
   if (payload.select) {
-    selections.select = {};
+    const select = {
+      select: {},
+    };
 
     if (payload.select.indexOf(',') > -1) {
       payload.select.split(',').forEach((item) => {
-        selections.select[item] = true;
+        const computedFieldIndex: number = computedFields.findIndex((field) => item === field.name);
+        if (computedFieldIndex > -1) {
+          computedFields[computedFieldIndex].compute = true;
+        } else {
+          Object.assign(select.select, { [item]: true });
+        }
       });
     } else {
-      selections.select[payload.select] = true;
+      Object.assign(select.select, { [payload.select]: true });
     }
+
+    Object.assign(selections, select);
   }
 
-  const stats = await prisma.stats.findMany({
+  const stats = (await prisma.stats.findMany({
     take: parseInt(payload.limit, 10) || 1,
     orderBy: [{ created_at: payload.order || 'desc' }],
     where: {
@@ -53,7 +84,22 @@ export const getProjectStatsById = async (payload: Payload) => {
       ...lastPreviousDay,
     },
     ...selections,
-  });
+  })) as Stats[];
+
+  const getComputedFields: ComputedField[] = computedFields.filter((field: ComputedField) => field.compute === true);
+
+  if (getComputedFields.length) {
+    return stats.map((stat) => {
+      const computedStat: Stats = { ...stat };
+
+      getComputedFields.forEach((field) => {
+        const computedValue = field.generate(computedStat);
+        Object.assign(computedStat, { [field.name]: computedValue });
+      });
+
+      return computedStat;
+    });
+  }
 
   return stats;
 };
